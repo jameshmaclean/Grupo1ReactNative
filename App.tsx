09263f50fb5c5258c5a716e34ffd3838c2f426c1
jameshmaclean <james.hector.maclean@gmail.com>
2023-06-22 18:42:17 -0300

    Pagina de perfil

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Carrinho from './src/screens/Carrinho';
+import Profile from './src/screens/Perfil';
 
 export default function App() {
   return (
     // <View style={styles.container}>
     <View>
-      <Carrinho/>
+      {/* <Carrinho/> */}
+      <Profile/>
       <StatusBar style="auto" />
     </View>
   );
